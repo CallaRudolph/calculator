fec3abbe1e0d932c95b62236c1c9ff3157b05796
Brittany Kerr and Calla Rudolph <calla@>
2017-05-31 15:08:50 -0700

    add HTML and JS elements for form

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,14 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number, yo:"));
-var number2 = parseInt(prompt("Enter another goshdarn number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// }
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// }
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// }
-
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(divide(number1, number2));
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
