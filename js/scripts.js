d395e9cc3fd25b7ffd65459fe7dd25f629941041
Brittany Kerr and Calla Rudolph <calla@>
2017-05-31 15:33:54 -0700

    add comments to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -13,6 +15,9 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+
+// User interface (or front-end) logic:
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
