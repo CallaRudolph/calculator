a45508d6891c544efb2ec85a6f14530f2b07e41c
Brittany Kerr and Calla Rudolph <calla@>
2017-05-31 14:57:10 -0700

    restore add as primary function in js, add Calculator title and h1 to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number, yo:"));
-// var number2 = parseInt(prompt("Enter another goshdarn number:"));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number, yo:"));
+var number2 = parseInt(prompt("Enter another goshdarn number:"));
+
+alert(add(number1, number2));
 
 // var subtract = function(number1, number2) {
 //   return number1 - number2;
@@ -19,11 +21,11 @@
 // var number1 = parseInt(prompt("Enter a number:"));
 // var number2 = parseInt(prompt("Enter another number:"));
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-}
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// }
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(divide(number1, number2));
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// alert(divide(number1, number2));
